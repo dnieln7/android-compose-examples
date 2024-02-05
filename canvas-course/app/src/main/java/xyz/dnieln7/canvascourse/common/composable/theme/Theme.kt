2bfafd21d5a4fcea1db62b1d810f00ae48d11b3f
Daniel <dnieln7@gmail.com>
2024-02-05 15:35:34 -0600

    Formatted

--- canvas-course/app/src/main/java/xyz/dnieln7/canvascourse/common/composable/theme/Theme.kt
+++ canvas-course/app/src/main/java/xyz/dnieln7/canvascourse/common/composable/theme/Theme.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import xyz.dnieln7.canvascourse.common.composable.theme.Typography
 
 private val lightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
