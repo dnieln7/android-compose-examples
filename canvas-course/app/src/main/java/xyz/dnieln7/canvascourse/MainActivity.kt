d0ebf93069672d78a118a0b46f18a5a80f46d4fd
Daniel <dnieln7@gmail.com>
2024-02-08 17:00:21 -0600

    Added Content to display composables

--- canvas-course/app/src/main/java/xyz/dnieln7/canvascourse/MainActivity.kt
+++ canvas-course/app/src/main/java/xyz/dnieln7/canvascourse/MainActivity.kt
@@ -3,10 +3,32 @@ package xyz.dnieln7.canvascourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import xyz.dnieln7.canvascourse.common.composable.custom.Clock
 import xyz.dnieln7.canvascourse.common.composable.theme.CVTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +43,72 @@ class MainActivity : ComponentActivity() {
 
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF50924E)),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .background(Color.White)
+                .align(Alignment.BottomCenter),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(modifier = Modifier.padding(20.dp)) {
+                Clock(radius = 200.dp)
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+            ) {
+                Icon(imageVector = Icons.Outlined.Person, contentDescription = null)
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Account Detail")
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Settings,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Settings")
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+            ) {
+                Icon(imageVector = Icons.Outlined.Info, contentDescription = null)
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Contact us")
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+            ) {
+                Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = "Blocked Users")
+            }
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
